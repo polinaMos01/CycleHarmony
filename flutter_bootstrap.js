886dbd54c7bc37e544a6bcd55c260a053afdc988
polinaMos01 <polinaMos01@users.noreply.github.com>
2026-08-21 06:03:08 +0000

    deploy: d024c59e8017b71b5e9ca5918e6836d6cd65b51a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "302737785" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3965833368" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
